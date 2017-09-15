c4652e7cb2b93a0473a16e6241566ea1bff2b67d
Ben Castan <ben@castanfamily.com>
2017-09-16 07:14:15 +1000

    Tweaks and tidy ups

--- program.py
+++ program.py
@@ -10,15 +10,24 @@ num_c = int(input(' Enter C '))
 
 d = num_b**2 - 4*num_a*num_c # discriminant
 
+
+def one_solution(a, b, d):
+    x = (-b + math.sqrt(d)) / 2 * a
+    return x
+
+
+def two_solution(a, b, c, d):
+    x1 = (-b+math.sqrt((d))) / (2 * a)
+    x2 = (-b-math.sqrt((d))) / (2 * a)
+    return x1, x2
+
+
 if d < 0:
     print('This equation has no real solution!')
 elif d == 0:
-    x = (-num_b + math.sqrt(d)) / 2 * num_a
-    print('This equation has one solution: ', x)
+    print('This equation has one solution: {}' .format(one_solution(num_a, num_b, d)))
 else:
-    x1 = (-num_b+math.sqrt((d))) / (2 * num_a)
-    x2 = (-num_b-math.sqrt((d))) / (2 * num_a)
-
-    print('This equation has two solutions: {} or {}'.format(str(x1), str(x2)))
+    x, y = two_solution(num_a, num_b, num_c, d)
+    print('This equation has two solutions: {} or {}'.format(x, y))
 
 
